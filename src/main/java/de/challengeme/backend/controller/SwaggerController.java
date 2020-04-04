de17e49d3fe0443425939d4843878dd7f0a68767
Volker Gronau <vjay@gmx.net>
2020-04-04 20:02:54 +0200

    changes for demo system

--- src/main/java/de/challengeme/backend/controller/SwaggerController.java
+++ src/main/java/de/challengeme/backend/controller/SwaggerController.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
-	@RequestMapping("/")
+public class SwaggerController {
+	@RequestMapping("/swagger")
 	public String home() {
 		return "redirect:/swagger-ui.html";
 	}
