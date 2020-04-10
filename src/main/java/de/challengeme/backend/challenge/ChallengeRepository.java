26a2bf12a2b0ae3b26e4513316722c5b610df4a6
Volker Gronau <Volker.Gronau@dimensiondata.com>
2020-04-10 09:39:25 +0200

    only return challenges one can do alone as random challenges only return imported challenges for now

--- src/main/java/de/challengeme/backend/challenge/ChallengeRepository.java
+++ src/main/java/de/challengeme/backend/challenge/ChallengeRepository.java
@@ -7,13 +7,13 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
 
-	@Query(value = "SELECT * FROM challenges WHERE deleted = false AND category = :category AND id NOT IN(SELECT challenge_id FROM challengeresults WHERE user_id=:userId) ORDER BY rand() LIMIT 1", nativeQuery = true)
+	@Query(value = "SELECT * FROM challenges WHERE deleted = false AND kind = 'self' AND created_by_import = true AND category = :category AND id NOT IN(SELECT challenge_id FROM challengeresults WHERE user_id=:userId) ORDER BY rand() LIMIT 1", nativeQuery = true)
 	public Challenge getRandomChallenge(String category, long userId);
 
-	@Query(value = "SELECT * FROM challenges WHERE deleted = false AND category = :category ORDER BY rand() LIMIT 1", nativeQuery = true)
+	@Query(value = "SELECT * FROM challenges WHERE deleted = false AND kind = 'self' AND created_by_import = true AND category = :category ORDER BY rand() LIMIT 1", nativeQuery = true)
 	public Challenge getRandomChallenge(String category);
 
-	@Query(value = "SELECT * FROM challenges WHERE deleted = false ORDER BY rand() LIMIT 1", nativeQuery = true)
+	@Query(value = "SELECT * FROM challenges WHERE deleted = false AND kind = 'self' AND created_by_import = true ORDER BY rand() LIMIT 1", nativeQuery = true)
 	public Challenge getRandomChallenge();
 
 	@Query(value = "SELECT * FROM challenges WHERE deleted = false AND created_by_user_id=:userId ORDER BY created_at", nativeQuery = true)
